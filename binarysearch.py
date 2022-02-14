c4bc5a1793e5fd815cadb5e1f794f48f9b972b0d
Will Long <completelyoffthegrid13@gmail.com>
2022-02-14 11:09:24 +0000

    INITAL

---
+++ binarysearch.py
@@ -0,0 +1,39 @@
+import random
+
+NUMS = []
+
+def SEARCHING_ALGORITHM():
+    FOUND = False
+    LOWERPART = 0
+    UPPERPART = len(NUMS) - 1
+    COUNT = 0
+    LENGTH = random.randint(1, 5000)
+
+    for i in range(LENGTH):
+        rnum = random.randint(0, 5000)
+        NUMS.append(rnum)
+
+    NUMS = list(dict.fromkeys(NUMS))
+    NUMS.sort()
+    print(NUMS)
+
+    VALUE = int(input('>> '))
+    while FOUND == False:
+        MID = int((LOWERPART + UPPERPART)) / 2
+        MID = round(MID)
+        if NUMS[MID] == VALUE:
+            print(f'Found At Array Index{MID} With {COUNT} Attempts')
+            FOUND = True
+
+        if NUMS[MID] > VALUE:
+            UPPERPART -= 1; 
+            COUNT += 1
+        else:
+           LOWERPART += 1
+           COUNT += 1
+
+        if COUNT > len(NUMS) * 2:
+            print(f'Value Not Found in Array')
+            break
+
+SEARCHING_ALGORITHM()
